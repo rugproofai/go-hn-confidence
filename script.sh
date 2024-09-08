6bbacf018ca6b2128201fc45d3e7c8f819d47868
tomj <tomj@tomj.com>
2024-09-08 16:08:18 +0800

    build: 2024-09-08 16:08:18

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/zsh
+
+cd "$(dirname "$0")"
+# Run the Go program
+go run hnconf.go
+
+# Get the current timestamp
+current_time=$(date "+%Y-%m-%d %H:%M:%S")
+
+# Commit changes with the timestamp
+git add .
+git commit -m "build: $current_time"
+
+# Push changes to remote repository
+git push
